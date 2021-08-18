50639ff4e0cce571a151ae7ebcb9c52a8a2fd586
fatmadogann <fatmadoganevs@gmail.com>
2021-08-18 16:56:42 +0200

    Designed Style

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ function displayForecast(response) {
 
   let forecastHTML = `<div class ="row">`;
   forecast.forEach(function (forecastDay, index) {
-    if (index < 6) {
+    if (index < 5) {
       forecastHTML =
         forecastHTML +
         `
